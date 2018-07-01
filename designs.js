b5da9c3da05c89c442898bef78437c99872f6a0e
Funto Daramola <fundaramola2013@gmail.com>
2018-07-01 20:31:58 +0100

    feat: code for som manipulation added

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+let height, width, color;
 
 function makeGrid() {
 
-// Your code goes here!
+    height = $("#inputHeight").val();
+    width = $("#inputWidth").val();
+        for (let x = 0; x < height; x++) {
+            $('#pixelCanvas').append('<tr></tr>');
+        }
+        for (let y = 0; y < width; y++) {
+            $('#pixelCanvas tr').each(function () {
+                $(this).append('<td></td>');
+            });
+        }
+        
+}
 
+
+$(document).on("mousedown", "tr td", function () {
+
+    color = $('#colorPicker').val();
+    $(this).css('background-color', color);
+
+    $('tr td').bind("mousemove", function () {
+        color = $('#colorPicker').val();
+        $(this).css('background-color', color);
+    }).mouseup(function() {
+        $('td').unbind('mousemove');
+    });
+
+    $('tr td').on('dblclick',function () {
+        $(this).css('background-color', "#FFFFFF")
+    })
+});
+
+reset = $("#pixelCanvas").html();
+
+function remove() {
+    $("#pixelCanvas").html(reset);
 }
